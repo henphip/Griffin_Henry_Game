e12226e27a30448fd282f19e66f1f09a5dd4aa3d
gtarpenning <gtarpenning@gmail.com>
2016-12-28 22:34:54 -0800

    gud

--- griffin.py
+++ griffin.py
@@ -1,6 +1,9 @@
 import datetime
 import pygame
 
+# TODO:
+# 1. Change the xy coordinate variables from 2 to 1 dict
+
 screen_default = raw_input('Would you like to use default screen dimensions? (y or n)')
 
 if screen_default.lower() in ['y', 'yes', 'sure', 'yay']:
@@ -15,14 +18,18 @@ rect_1_x = 10
 rect_1_y = (screen_y-30)/2
 rect_2_x = screen_x-(10+30)
 rect_2_y = (screen_y-30)/2
+player1_score = 0
+player2_score = 0
+tagger = 1
 
 pygame.init()
 screen = pygame.display.set_mode((screen_x, screen_y))
 clock = pygame.time.Clock()
+myfont = pygame.font.SysFont("monospace", 50)
 
 done = False
 while not done:
-    screen.fill((0, 0, 0))
+    screen.fill((255, 255, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
@@ -32,18 +39,31 @@ while not done:
     if pressed[pygame.K_BACKSPACE]:
         done = True
 
-    if pressed[pygame.K_UP] and rect_1_y > 5: rect_1_y -= 30
-    if pressed[pygame.K_DOWN] and rect_1_y < screen_y-30: rect_1_y += 30
-    if pressed[pygame.K_RIGHT] and rect_1_x < screen_x-30: rect_1_x += 30
-    if pressed[pygame.K_LEFT] and rect_1_x > 5: rect_1_x -= 30
+    if pressed[pygame.K_UP] and rect_1_y > 5: rect_1_y -= 15
+    if pressed[pygame.K_DOWN] and rect_1_y < screen_y-30: rect_1_y += 15
+    if pressed[pygame.K_RIGHT] and rect_1_x < screen_x-30: rect_1_x += 15
+    if pressed[pygame.K_LEFT] and rect_1_x > 5: rect_1_x -= 15
 
-    if pressed[pygame.K_w] and rect_2_y > 5: rect_2_y -= 30
-    if pressed[pygame.K_s] and rect_2_y < screen_y-30: rect_2_y += 30
-    if pressed[pygame.K_d] and rect_2_x < screen_x-30: rect_2_x += 30
-    if pressed[pygame.K_a] and rect_2_x > 5: rect_2_x -= 30
+    if pressed[pygame.K_w] and rect_2_y > 5: rect_2_y -= 15
+    if pressed[pygame.K_s] and rect_2_y < screen_y-30: rect_2_y += 15
+    if pressed[pygame.K_d] and rect_2_x < screen_x-30: rect_2_x += 15
+    if pressed[pygame.K_a] and rect_2_x > 5: rect_2_x -= 15
+
+    if abs(rect_1_y-rect_2_y) < 15 and abs(rect_1_x-rect_2_x) < 15:
+        if tagger == 1:
+            player1_score += 1
+        if tagger == 2:
+            player2_score += 1
+
+        rect_1_x = 10
+        rect_1_y = (screen_y-30)/2
+        rect_2_x = screen_x-(10+30)
+        rect_2_y = (screen_y-30)/2
 
     pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(rect_1_x, rect_1_y, 30, 30))
     pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(rect_2_x, rect_2_y, 30, 30))
+    label1 = myfont.render(("Player 1 Score: " + str(player1_score) + '      Player 2 Score: ' + str(player2_score)), 1, (0,0,0))
+    screen.blit(label1, (50, 10))
 
     pygame.display.flip()
     clock.tick(60)
